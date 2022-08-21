98173d175619cacfb89ed559bf38d56f9fc58314
David Danier <danier@team23.de>
2022-08-21 16:22:55 +0200

    Tests now run at least

--- fastapi_django_test/api/v1/tests/test_something.py
+++ fastapi_django_test/api/v1/tests/test_something.py
@@ -3,10 +3,23 @@ from httpx import AsyncClient
 from dirty_equals import IsPartialDict, Contains, IsAnyStr
 
 from ....main import app
+from ....something.models import Something
+
+
+@pytest.fixture
+@pytest.mark.django_db
+@pytest.mark.usefixtures('django_db_setup')
+def create_somethings():
+    print("Creating somethings")
+    Something.objects.create(
+        name='Something 1',
+    )
 
 
 @pytest.mark.anyio
-async def test_hello_world():
+@pytest.mark.django_db
+@pytest.mark.usefixtures('create_somethings')
+async def test_hello_world(django_db_blocker):
     async with AsyncClient(app=app, base_url="http://test") as ac:
         response = await ac.get("/api/v1/somethings/")
     assert response.status_code == 200
