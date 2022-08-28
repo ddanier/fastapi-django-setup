dedcee8b3dff7d36f7c696c6e8e5323fee1603b3
David Danier <danier@team23.de>
2022-08-28 13:30:33 +0200

    Map way more fields

--- fastapi_django_test/utils/models/django.py
+++ fastapi_django_test/utils/models/django.py
@@ -1,4 +1,7 @@
-from typing import Generic, Optional, Type, TypeVar, cast
+import datetime
+import decimal
+from typing import Any, Callable, Generic, Type, TypeVar, cast
+from uuid import UUID
 
 import pydantic
 from django.db import models
@@ -18,14 +21,37 @@ class DjangoModelBase(pydantic.BaseModel, Generic[DjangoModelT]):
         return cls.parse_obj(model_to_dict(obj))
 
 
-FIELD_TYPE_MAP = {
-    models.AutoField: int,
-    models.SmallAutoField: int,
-    models.BigAutoField: int,
-    models.CharField: str,
-    models.IntegerField: int,
-    models.TextField: str,
-    # TODO: Continue...
+FIELD_TYPE_MAP: dict[type[models.Field], tuple[type, Callable[[models.Field], dict[str, Any]] | None] | None] = {
+    models.AutoField: (int, None),
+    models.SmallAutoField: (int, None),
+    models.BigAutoField: (int, None),
+    models.CharField: (str, lambda f: {'max_length': f.max_length}),
+    models.IntegerField: (int, None),
+    models.PositiveIntegerField: (pydantic.PositiveInt, None),
+    models.SmallIntegerField: (int, None),
+    models.PositiveSmallIntegerField: (pydantic.PositiveInt, None),
+    models.BigIntegerField: (int, None),
+    models.PositiveBigIntegerField: (pydantic.PositiveInt, None),
+    models.TextField: (str, lambda f: {'max_length': f.max_length}),
+    models.BinaryField: (bytes, None),
+    models.BooleanField: (bool, None),
+    models.DateField: (datetime.date, None),
+    models.DateTimeField: (datetime.datetime, None),
+    models.TimeField: (datetime.time, None),
+    models.DurationField: (datetime.timedelta, None),
+    models.DecimalField: (decimal.Decimal, None),
+    models.EmailField: (pydantic.EmailStr, None),
+    # TODO: What fits best here? models.FileField: (pydantic.FilePath, None),
+    # TODO: What fits best here? models.FilePathField: (pydantic.FilePath, None),
+    # TODO: What fits best here? models.ImageField: (pydantic.FilePath, None),
+    models.GenericIPAddressField: (pydantic.IPvAnyAddress, None),
+    models.JSONField: (dict[str, Any] | list, None),
+    models.SlugField: (str, None),
+    models.URLField: (pydantic.AnyUrl, None),
+    models.UUIDField: (UUID, None),
+    models.ForeignKey: (int, None),  # This might be tricky for more complex cases of IDs
+    models.OneToOneField: (int, None),  # This might be tricky for more complex cases of IDs
+    models.ManyToManyField: None,  # skip
 }
 
 
@@ -36,8 +62,12 @@ def django_to_pydantic_model(
 
     for field in model_class._meta.get_fields(include_hidden=False):
         pydantic_type = None
+        pydantic_params_callback = None
         if field.__class__ in FIELD_TYPE_MAP:
-            pydantic_type = FIELD_TYPE_MAP[field.__class__]
+            pydantic_config = FIELD_TYPE_MAP[field.__class__]
+            if pydantic_config is None:
+                continue  # skip field
+            pydantic_type, pydantic_params_callback = pydantic_config
 
         if pydantic_type is None:
             for django_field_type, pydantic_type_ in FIELD_TYPE_MAP.items():
@@ -51,16 +81,20 @@ def django_to_pydantic_model(
 
         pydantic_default = ...
         if field.null:
-            pydantic_type = Optional[pydantic_type]
-            pydantic_default = None
+            pydantic_type = pydantic_type | None
+            if field.blank:
+                pydantic_default = None
 
-        # TODO: Handle max_length etc. ;-)
+        pydantic_params = {}
+        if pydantic_params_callback is not None:
+            pydantic_params = pydantic_params_callback(field)
         pydantic_fields[field.name] = (
             pydantic_type,
             pydantic.Field(
                 pydantic_default,
                 title=str(field.verbose_name),
                 description=str(field.help_text),
+                **pydantic_params,
             ),
         )
 
