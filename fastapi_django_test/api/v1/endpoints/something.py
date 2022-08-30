a43763df806e4929063cafc96899f04cf432a2ac
David Danier <danier@team23.de>
2022-08-30 21:51:20 +0200

    Use async already ;-)

--- fastapi_django_test/api/v1/endpoints/something.py
+++ fastapi_django_test/api/v1/endpoints/something.py
@@ -9,9 +9,9 @@ router = APIRouter()
 
 
 @router.get("/somethings/", response_model=List[SomethingDTO])
-def get_somethings() -> List[SomethingDTO]:
+async def get_somethings() -> List[SomethingDTO]:
     return [
         SomethingDTO.from_django(something)
-        for something
+        async for something
         in Something.objects.all()
     ]
