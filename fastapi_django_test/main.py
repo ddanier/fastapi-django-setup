3a0851ba525b27f3ffac8e2f09f2802f6d54956a
David Danier <danier@team23.de>
2022-09-19 15:22:02 +0200

    feat: Skip isort

--- fastapi_django_test/main.py
+++ fastapi_django_test/main.py
@@ -1,9 +1,9 @@
+from django.conf import settings
+from django.contrib.staticfiles.handlers import StaticFilesHandler
 from fastapi import FastAPI
 from fastapi.middleware.wsgi import WSGIMiddleware
 
-from django.contrib.staticfiles.handlers import StaticFilesHandler
-from django.conf import settings
-
+# isort: off
 from .wsgi import application as django_app
 from .api.v1 import router as v1_router
 from .utils.api.route_names import use_route_names_as_operation_ids
