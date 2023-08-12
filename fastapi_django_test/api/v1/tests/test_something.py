4959cdea808e8c5e548e556d7a5233deabaab496
David Danier <danier@team23.de>
2023-08-12 10:13:18 +0200

    test: 🚨 Use v1 app in tests for v1 API

--- fastapi_django_test/api/v1/tests/test_something.py
+++ fastapi_django_test/api/v1/tests/test_something.py
@@ -2,8 +2,17 @@ import pytest
 from dirty_equals import Contains, IsPartialDict, IsPositiveInt
 from httpx import AsyncClient
 
-from ....main import app
-from ....something.models import Something
+from fastapi_django_test.something.models import Something
+
+
+@pytest.fixture()
+def app():
+    # Always import the app inside fixtures, so setup of the app is only done if
+    # requested by the selected tests - this may reduce the time testing single files
+    # by A LOT.
+    from .. import api_v1
+
+    return api_v1
 
 
 @pytest.fixture()
@@ -17,9 +26,9 @@ async def create_somethings():  # noqa: PT004
 @pytest.mark.anyio()
 @pytest.mark.django_db()
 @pytest.mark.usefixtures('create_somethings', 'transactional_db')
-async def test_hello_world():
+async def test_hello_world(app):
     async with AsyncClient(app=app, base_url="http://test") as ac:
-        response = await ac.get("/api/v1/somethings/")
+        response = await ac.get("/somethings/")
 
     assert response.status_code == 200
     assert response.json() == Contains(
--- fastapi_django_test/something/tests/test_django.py
+++ fastapi_django_test/something/tests/test_django.py
@@ -3,11 +3,11 @@ from django.test import TestCase
 from fastapi_django_test.something.models import Something
 
 
-class AnimalTestCase(TestCase):
+class SomethingTestCase(TestCase):
     def setUp(self):
         Something.objects.create(name="A")
         Something.objects.create(name="B")
 
-    def test_animals_can_speak(self):
+    def test_get_somethings(self):
         Something.objects.get(name="A")
         Something.objects.get(name="B")
