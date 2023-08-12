f2b1239dd6defc62d387b93bb5dcd6e821ae158a
David Danier <danier@team23.de>
2023-08-12 10:07:39 +0200

    refactor: 📦 Beginning to move FastAPI<->Django utils to own module

---
+++ fastapi_django/README.md
@@ -0,0 +1,1 @@
+This is a separate folder to prepare the things we do here to becoma a nice library. 
--- fastapi_django_test/something/dto.py
+++ fastapi_django_test/something/dto.py
@@ -1,5 +1,5 @@
+from fastapi_django.models import django_to_pydantic_model
 from fastapi_django_test.something.models import Something
-from fastapi_django_test.utils.models.django import django_to_pydantic_model
 
 
 class SomethingDTO(django_to_pydantic_model(Something)):  # type: ignore
