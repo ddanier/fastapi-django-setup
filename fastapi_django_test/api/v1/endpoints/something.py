24c26a4e4b93a7f5759776915d90930661775f02
David Danier <danier@team23.de>
2023-08-13 19:47:42 +0200

    test: 🚨 Add failing test showing DB transaction issue

--- fastapi_django_test/api/v1/endpoints/something.py
+++ fastapi_django_test/api/v1/endpoints/something.py
@@ -1,5 +1,6 @@
+from typing import Annotated
 
-from fastapi import APIRouter
+from fastapi import APIRouter, Path
 
 from fastapi_django_test.something.dto import SomethingDTO
 from fastapi_django_test.something.models import Something
@@ -7,6 +8,8 @@ from fastapi_django_test.something.models import Something
 router = APIRouter()
 
 
+# Note: You should NEVER just return a list, this is just for demo purposes, use a
+#       object containing the list instead.
 @router.get("/somethings/", response_model=list[SomethingDTO])
 async def get_somethings() -> list[SomethingDTO]:
     return [
@@ -14,3 +17,12 @@ async def get_somethings() -> list[SomethingDTO]:
         async for something
         in Something.objects.all()
     ]
+
+
+@router.get("/somethings/{id}/", response_model=SomethingDTO)
+async def get_something_by_id(
+    id_: Annotated[int, Path(..., alias="id")],
+) -> list[SomethingDTO]:
+    something = await Something.objects.aget(id=id_)
+
+    return SomethingDTO.from_django(something)
--- fastapi_django_test/api/v1/tests/test_something.py
+++ fastapi_django_test/api/v1/tests/test_something.py
@@ -1,5 +1,5 @@
 import pytest
-from dirty_equals import Contains, IsPartialDict, IsPositiveInt
+from dirty_equals import Contains, HasLen, IsPartialDict, IsPositiveInt
 from httpx import AsyncClient
 
 from fastapi_django_test.something.models import Something
@@ -20,20 +20,34 @@ def app():
     return api_v1
 
 
+SOMETHING_ID_1 = 1
+
+
 @pytest.fixture()
 async def create_somethings():
     await Something.objects.acreate(
+        id=SOMETHING_ID_1,
         name='Something 1',
     )
 
 
 @pytest.mark.anyio()
-@pytest.mark.usefixtures('create_somethings', 'transactional_db')
-async def test_hello_world(app):
+@pytest.mark.usefixtures('create_somethings')
+async def test_somethings_list(app):
     async with AsyncClient(app=app, base_url="http://test") as ac:
         response = await ac.get("/somethings/")
 
     assert response.status_code == 200
     assert response.json() == Contains(
-        IsPartialDict(id=IsPositiveInt),
-    )
+        IsPartialDict(id=SOMETHING_ID_1),
+    ) & HasLen(1)
+
+
+@pytest.mark.anyio()
+@pytest.mark.usefixtures('create_somethings')
+async def test_something_by_id(app):
+    async with AsyncClient(app=app, base_url="http://test") as ac:
+        response = await ac.get(f"/somethings/{SOMETHING_ID_1}/")
+
+    assert response.status_code == 200
+    assert response.json() == IsPartialDict(id=SOMETHING_ID_1)
